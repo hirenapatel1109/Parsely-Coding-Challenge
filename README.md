ea2a9eb1c33d23bf2b1c2f73cc1068197119b46a
Hiren Patel [I] <hirepatel@tesla.com>
2022-06-19 18:11:17 -0400

    adding readme

---
+++ README.md
@@ -0,0 +1,22 @@
+# Puzzle creater and anagram solver
+
+## How to deploy
+
+### Install Minikube (if running locally)
+
+- Follow instructions here: https://minikube.sigs.k8s.io/docs/start/
+
+### Apply Kubernetes deployment file
+
+- run `kubectl apply -f deployment_kubernetes.yaml`
+- This will deploy the kubernetes
+  -- Ingress
+  -- Service
+  -- Deployment
+  -- Pods (3 replicas)
+
+### Access the website
+
+_Keep in mind that the URL depends on how you set it up. If you use minikube and your minikube cluster runs on http://192.168.99.102/ locally, go to this website_
+
+- Go to http://192.168.99.102/anagrams?prompt=pots
--- codetest.py
+++ codetest.py
@@ -11,6 +11,9 @@ DICTIONARY = open("./sowpods")
 app = Flask(__name__)
 
 
+""" Prepare the data """
+
+
 def cleanData(data):
     words = {}  # length: [words, ...]
     for word in DICTIONARY:
@@ -25,9 +28,15 @@ def cleanData(data):
 words = cleanData(DICTIONARY)
 
 
+""" Front page of the website """
+
+
 @app.route("/")
 def index():
     return "hello world!!!"
+
+
+""" Find all the anagrams of a word endpoint """
 
 
 @app.route("/anagrams")
@@ -38,9 +47,10 @@ def anagrams():
     return json.dumps({"result": find_anagrams(prompt.upper())})
 
 
+""" Find all the anagrams of a word """
+
+
 def find_anagrams(prompt):
-    # call your solver entrypoint here, and return the result from this
-    # function.
     potentialWords = words[len(prompt)]
     sortedPrompt = ''.join(sorted(prompt))
     print(prompt, sortedPrompt)
@@ -51,6 +61,54 @@ def find_anagrams(prompt):
             answer.append(word)
     print(answer)
     return answer
+
+
+""" Generate a puzzle based on a diffculty of 0 - 2 """
+
+
+@app.route("/generatePuzzle")
+def generatePuzzle():
+    diff = request.args.get("difficulty")
+    if diff is None or len(diff) <= 0:
+        return "Please add a valid diff in generatePuzzle. Example: /generatePuzzle?difficulty=1. difficulty is between 0 - 2", 400
+    if diff < 0 or diff > 2:
+        return "ERROR: Please use 0-2 difficulty"
+    diff = int(diff)
+    randomList = None
+    wordLens = list(words.keys())
+    if diff == 0:
+        randomList = choice(wordLens[:len(words) // 3])
+    elif diff == 1:
+        randomList = choice(wordLens[len(words) // 3:len(words) * 2 // 3])
+    elif diff == 2:
+        randomList = choice(wordLens[2 * len(words) // 3:])
+    else:
+        return "Please add a valid diff in generatePuzzle. Example: /generatePuzzle?difficulty=1. difficulty is between 0 - 2", 400
+    randomWord = choice(words[randomList])
+    randomWordList = list(randomWord)
+    shuffle(randomWordList)
+    randomWordNew = "".join(randomWordList)
+    return json.dumps({"result": randomWordNew})
+
+
+""" Verify a puzzle equates to a guess """
+
+
+@app.route("/verify")
+def verify():
+    puzzle = request.args.get("puzzle")
+    if puzzle is None or len(puzzle) <= 0:
+        return "Please add a valid puzzle in verify. Example: /verify?puzzle=lleho&guess=hello.", 400
+    guess = request.args.get("guess")
+    if guess is None or len(guess) <= 0:
+        return "Please add a valid guess in verify. Example: /verify?puzzle=lleho&guess=hello.", 400
+    puzzle = list(puzzle)
+    for char in guess:
+        if char not in puzzle:
+            return False
+        else:
+            puzzle.remove(char)
+    return json.dumps({"result": len(puzzle) == 0})
 
 
 if __name__ == '__main__':
